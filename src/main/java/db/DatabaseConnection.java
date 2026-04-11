7b93e4d69cd98440ebe576da9179c758a947b5b9
Tuukka Uusitalo <tuukka.uusitalo02@gmail.com>
2026-04-11 23:55:35 +0300

    added application.properties file and added it to gitignore. I took moved the credentials there

--- src/main/java/db/DatabaseConnection.java
+++ src/main/java/db/DatabaseConnection.java
@@ -2,15 +2,33 @@ package db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
+import java.io.InputStream;
 
 public class DatabaseConnection {
 
-    private static final String URL =
-            "jdbc:mariadb://localhost:3306/shopping_cart_localization";
-    private static final String USER = "calc_user";
-    private static final String PASSWORD = "Test12"; //
+    private static final Properties props = new Properties();
+
+    static {
+        try (InputStream is = DatabaseConnection.class
+                .getClassLoader()
+                .getResourceAsStream("application.properties")) {
+
+            if (is == null) {
+                throw new RuntimeException("application.properties not found");
+            }
+            props.load(is);
+
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to load database configuration", e);
+        }
+    }
 
     public static Connection getConnection() throws Exception {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        String url = props.getProperty("db.url");
+        String user = props.getProperty("db.user");
+        String password = props.getProperty("db.password");
+
+        return DriverManager.getConnection(url, user, password);
     }
 }
